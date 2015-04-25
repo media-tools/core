5a1901fa77b69df7e1ef16d31f0f2bc50f2c59f3
Tobias Schulz <tobias.schulz186@gmail.com>
2015-04-25 17:43:36 +0200

    nuget release 0.1.36

--- Core/pack.sh
+++ Core/pack.sh
@@ -1,3 +1,6 @@
+# git
+git pull
+
 # build
 xbuild /p:Configuration=Release Core.sln
 
@@ -6,6 +9,7 @@ for x in *.nuspec
 do
 	perl -pi.bak -e 's/<version>([0-9]+)\.([0-9]+)\.([0-9]+)<\/version>/"<version>$1.$2.".($3+1)."<\/version>"/ge;' "$x"
 done
+MAIN_VERSION=$(grep "<version>" Core.Common.nuspec | perl -n -e 's/[^0-9.]+//g; print')
 
 # pack
 rm -rf nuget-out
@@ -15,6 +19,14 @@ do
 	nuget pack "$x" -Prop Configuration=Release -OutputDirectory nuget-out
 done
 
+# git
+git add --all
+git commit -a -m "nuget release $MAIN_VERSION"
+git push
+
 # push
-nuget push nuget-out/*.nupkg
+for x in nuget-out/*.nupkg
+do
+	nuget push "$x"
+done
 

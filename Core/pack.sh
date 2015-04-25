e436a968fb408079272c86d8e08ffd881cedc370
Tobias Schulz <tobias.schulz186@gmail.com>
2015-04-25 16:56:51 +0200

    pack script

--- Core/pack.sh
+++ Core/pack.sh
@@ -1,15 +1,20 @@
+# build
 xbuild /p:Configuration=Release Core.sln
+
+# nuget version
+for x in *.nuspec
+do
+	perl -pi.bak -e 's/<version>([0-9]+)\.([0-9]+)\.([0-9]+)<\/version>/"<version>$1.$2.".($3+1)."<\/version>"/ge;' "$x"
+done
+
+# pack
 rm -rf nuget-out
 mkdir nuget-out
-nuget pack Core.nuspec -Prop Configuration=Release -OutputDirectory nuget-out
+for x in *.nuspec
+do
+	nuget pack "$x" -Prop Configuration=Release -OutputDirectory nuget-out
+done
+
+# push
 nuget push nuget-out/*.nupkg
 
-exit 0
-
-call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;TragetFrameworkVersion=4.5
-call sn -R .\src\bin\Commons.Utils.dll .\src\FullKey.snk
-call sn -R .\src\bin\Commons.Collections.dll .\src\FullKey.snk
-call sn -R .\src\bin\Commons.Json.dll .\src\FullKey.snk
-call sn -R .\src\bin\Commons.Pool.dll .\src\FullKey.snk
-call src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe src\bin\Test.Commons.dll /xml src\bin\UnitTestResult.xml
-call nuget pack Commons.nuspec -OutputDirectory ".\src\bin"

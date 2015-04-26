9ae69a41051fdc7b25a3e460f81c67d3a913690d
Tobias Schulz <tobias.schulz186@gmail.com>
2015-04-26 19:00:18 +0200

    nuget release 0.1.57

--- Core.Tests/Core.Tests.Calendar.Google/Program.cs
+++ Core.Tests/Core.Tests.Calendar.Google/Program.cs
@@ -3,6 +3,7 @@ using Core.Calendar.Google;
 using Core.Common;
 using System.Reflection;
 using System.Globalization;
+using Google.Apis.Auth.OAuth2;
 
 namespace Core.Tests.Calendar.Google
 {
@@ -10,36 +11,9 @@ namespace Core.Tests.Calendar.Google
 	{
 		public static void Main (string[] args)
 		{
-			RedirectAssembly (shortName: "System.Net.Http.Primitives", targetVersion: new Version (2, 2, 28, 0), publicKeyToken: "b03f5f7f11d50a3a");
-
 			Logging.Enable ();
 
 			GoogleCalendarService google = new GoogleCalendarService (new TestConfig ());
-		}
-
-		///<summary>Adds an AssemblyResolve handler to redirect all attempts to load a specific assembly name to the specified version.</summary>
-		public static void RedirectAssembly (string shortName, Version targetVersion, string publicKeyToken)
-		{
-			ResolveEventHandler handler = null;
-
-			handler = (sender, args) => {
-				Console.WriteLine ("May redirect assembly load of " + args.Name);
-				// Use latest strong name & version when trying to load SDK assemblies
-				var requestedAssembly = new AssemblyName (args.Name);
-				if (requestedAssembly.Name != shortName)
-					return null;
-
-				Console.WriteLine ("Redirecting assembly load of " + args.Name + ",\tloaded by " + (args.RequestingAssembly == null ? "(unknown)" : args.RequestingAssembly.FullName));
-
-				requestedAssembly.Version = targetVersion;
-				requestedAssembly.SetPublicKeyToken (new AssemblyName ("x, PublicKeyToken=" + publicKeyToken).GetPublicKeyToken ());
-				requestedAssembly.CultureInfo = CultureInfo.InvariantCulture;
-
-				AppDomain.CurrentDomain.AssemblyResolve -= handler;
-
-				return Assembly.Load (requestedAssembly);
-			};
-			AppDomain.CurrentDomain.AssemblyResolve += handler;
 		}
 
 		public class TestConfig : IGoogleConfig
---
+++ Core/Core.Google.Auth.Desktop/GoogleWebAuth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+
+namespace Core.Google.Auth.Desktop
+{
+	public class GoogleWebAuth
+	{
+		internal readonly string ClientId;
+		internal readonly string ClientSecret;
+
+		public GoogleWebAuth (string clientId, string clientSecret)
+		{
+			ClientId = clientId;
+			ClientSecret = clientSecret;
+		}
+
+		public UserCredential Authorize (string googleUser)
+		{
+			return GoogleWebAuthorizationBroker.AuthorizeAsync (
+				new ClientSecrets {
+					ClientId = ClientId,
+					ClientSecret = ClientSecret
+				},
+				new[] { "https://www.googleapis.com/auth/calendar" },
+				googleUser, System.Threading.CancellationToken.None, 
+				new FileDataStore ("Drive.Auth.Store")
+			).Result;
+		}
+	}
+}
+

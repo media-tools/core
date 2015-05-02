ab2353f3ca78cf69bb1b463f5ce0bd1e691d84c1
Tobias Schulz <tobias.schulz186@gmail.com>
2015-05-03 01:31:35 +0200

    nuget release 0.1.94

---
+++ Core/Core.Markdown/Markdown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommonMark;
+
+namespace Core.Markdown
+{
+	public static class Markdown
+	{
+		public static async Task<string> MarkdownToHTML (string markdownCode)
+		{
+			string htmlCode = null;
+			await Task.Run (() => {
+				CommonMarkSettings settings = CommonMarkSettings.Default;
+				settings.OutputFormat = OutputFormat.Html;
+				settings.AdditionalFeatures = CommonMarkAdditionalFeatures.All;
+				htmlCode = CommonMarkConverter.Convert (source: markdownCode, settings: settings);
+			});
+			return htmlCode;
+		}
+	}
+}
+

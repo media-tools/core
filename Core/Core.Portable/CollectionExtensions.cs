6adfb9d0db4719c170716bdd71a569b9c938c22e
Tobias Schulz <tobias.schulz186@gmail.com>
2015-05-03 17:02:01 +0200

    nuget release 0.1.100

---
+++ Core/Core.Portable/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Common
+{
+	public static class CollectionExtensions
+	{
+		public static IEnumerable<T> TakeLast<T> (IEnumerable<T> source, int N)
+		{
+			return source.Skip (Math.Max (0, source.Count () - N));
+		}
+	}
+}
+

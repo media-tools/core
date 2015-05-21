3bfbcfb20ea868b24c6169eda11394f852c46fa5
Tobias Schulz <tobias.schulz186@gmail.com>
2015-05-21 11:19:43 +0200

    ...

--- Core/Core.Platform/Platform/FileAssociation.cs
+++ Core/Core.Platform/Platform/FileAssociation.cs
@@ -5,31 +5,38 @@ namespace Core.Platform
 {
 	public static class FileAssociation
 	{
-		public static void SetAssociation (string[] extensions, string description, string exePath, string iconPath)
+		public static void SetAssociation (string[] extensions, string id, string description, string exePath, string iconPath)
 		{
 			if (SystemInfo.IsRunningOnWindows ()) {
-				WindowsImpl.setAssociation (extensions: extensions, description: description, exePath: exePath, iconPath: iconPath);
+				WindowsImpl.setAssociation (extensions: extensions, id: id, description: description, exePath: exePath, iconPath: iconPath);
 			}
 		}
 
 		static class WindowsImpl
 		{
-			internal static void setAssociation (string[] extensions, string description, string exePath, string iconPath)
+			internal static void setAssociation (string[] extensions, string id, string description, string exePath, string iconPath)
 			{
+				var classes = Registry.CurrentUser.CreateSubKey (@"Software\Classes");
 				foreach (string extension in extensions) {
 					RegistryKey key = classes.CreateSubKey (extension);
+					key.SetValue ("", id);
+					key.Close ();
+				}
+
+				{
+					RegistryKey key = classes.CreateSubKey (id);
 					key.SetValue ("", description);
 					key.Close ();
 				}
 
 				if (exePath != null) {
-					RegistryKey key = classes.CreateSubKey (description + @"\Shell\Open\command");
+					RegistryKey key = classes.CreateSubKey (id + @"\Shell\Open\command");
 					key.SetValue ("", "\"" + exePath + "\" \"%L\"");
 					key.Close ();
 				}
 
 				if (iconPath != null) {
-					RegistryKey key = classes.CreateSubKey (description + @"\DefaultIcon");
+					RegistryKey key = classes.CreateSubKey (id + @"\DefaultIcon");
 					key.SetValue ("", iconPath);
 					key.Close ();
 				}

a85800290efed9c322b1f5e580e7da4de78c324d
Tobias Schulz <tobias.schulz186@gmail.com>
2015-05-23 16:58:07 +0200

    nuget release 0.1.144

---
+++ Core/commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# git
+git pull
+
+# nuget restore
+nuget restore
+
+# build
+xbuild /p:Configuration=Release Core.sln
+xbuild /p:Configuration=Debug Core.sln
+
+# git
+git add --all
+git commit -a
+git push

0867fcd92e300e4bd6e41dcf05eef7bfaaf5a1ba
Tobias Schulz <tobias.schulz186@gmail.com>
2015-05-28 03:05:06 +0200

    nuget release 0.1.168

---
+++ Core/Core.Portable/Math/PortableLocation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Core.Math
+{
+	public class PortableLocation
+	{
+		[JsonProperty ("lat")]
+		public double Latitude { get; set; } = 0;
+
+		[JsonProperty ("long")]
+		public double Longitude { get; set; } = 0;
+
+		[JsonProperty ("timestamp")]
+		public DateTime DateTime { get; set; } = default(DateTime);
+
+		public PortableLocation ()
+		{
+		}
+	}
+
+	public class PortableLocationCollection
+	{
+		[JsonProperty ("locations")]
+		List<PortableLocation> Locations { get; set; } = new List<PortableLocation> ();
+
+		public PortableLocationCollection ()
+		{
+		}
+
+		public void AddLocation (PortableLocation location)
+		{
+			Locations.Add (location);
+		}
+	}
+}
+
